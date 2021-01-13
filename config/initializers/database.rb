4ecdb0141c0575cd63fae5e5618aae00d5af1d5c
mrsdo <me@martidolce.com>
2021-01-13 14:45:28 -0500

    Figured out how to connect to MySQL
    Used corneal scaffold users name:string, etc --database=mysql2
    Added console.logs instead of movie
    Next: rake db:create (database: listings), db:migrate (table:users)

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,5 +1,5 @@
 require "yaml"
-settings = YAML::load_file("config/db.yml")
+settings = YAML::load_file("config/database.yml")
 # Sequel Configuration
 require "sequel"
  DB = Sequel.connect(settings[ENV['RACK_ENV']])
