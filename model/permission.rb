38f767b159543dc7f0b95679a9e1797ef3e1f650
Martinique Dolce <me@martidolce.com>
2021-01-18 18:18:29 -0500

    Documenting setting up and connecting to MySQL using Homebrew
    Updated documentation
    Added notes re: structure
    Installed MySQL via homebrew
    Setup MySQL security
    Validated connection to MySQL via terminal
    Created database.yml with dev, test and prod.
    Next:  ORM Mapping and create database

--- model/permission.rb
+++
@@ -1,6 +0,0 @@
-require 'sequel'
-
-class Permission < Sequel::Model
-  many_to_one :user
-  many_to_one :list
-end
--- model/user.rb
+++
@@ -1,9 +0,0 @@
-require 'sequel'
-
-class User < Sequel::Model
-  set_primary_key :id
-
-  one_to_many :items
-  one_to_many :permissions
-  one_to_many :logs
-end
