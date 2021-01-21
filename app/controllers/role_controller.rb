6860301c82e023c31d3a1281c0109a1f7400716e
mrsdo <me@martidolce.com>
2021-01-21 14:41:10 -0500

    updated roles form test
    Next: Merge into main

--- app/controllers/role_controller.rb
+++ app/controllers/role_controller.rb
@@ -8,5 +8,24 @@ class RoleController < ApplicationController
     erb :"/roles/index.html"
   end
 
+  # GET: 'roles/new' do
+  get "/roles/new" do
+    erb :'/roles/new.html'
+  end
+
+  # POST: '/roles/new' do
+
+
+
+
+
+  # PRIVATE
+  private
+
+  def self.connect
+
+
+  end
+
 
 end
